d4ef122c8c19d034f4407ca387a6d56df56b3808
akthonka <manneredmanul@gmail.com>
2023-12-17 15:48:30 +0100

    introduced multi-stage regex matching

--- modules/commons.py
+++ modules/commons.py
@@ -13,3 +13,13 @@ class Commons:
         with open(file_path, mode="r", encoding="utf-8") as file:
             reader = csv.DictReader(file)
             return {row["Name"]: row["Value"] for row in reader}
+
+    def alt_field_map(self, input_dir):
+        alt_field_map = {}
+        with open(input_dir, mode="r", encoding="utf-8") as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                for key, value in row.items():
+                    if value:  # Only add non-empty alternatives
+                        alt_field_map[value.lower()] = key
+        return alt_field_map
--- modules/form_filler.py
+++ modules/form_filler.py
@@ -1,5 +1,4 @@
-import os, csv, time
-from selenium.webdriver.common.by import By
+import os, re
 from modules.commons import Commons
 
 
@@ -16,6 +15,36 @@ class FormFiller:
         }
         self.values_to_select = []  # List of possible values to select
 
+    def match_field_name(self, web_label, alt_field_map):
+        # Normalize the label by removing non-alphanumeric characters and converting to lowercase
+        normalized_label = re.sub(r"[^a-z0-9]", "", web_label.lower())
+
+        # Exact match check
+        if normalized_label in alt_field_map:
+            foo = alt_field_map[normalized_label]
+            print(f"Matched standard: {foo}")
+            return foo
+
+        # Attempt to find a match using regex with word boundaries for more complete matches
+        # TODO: Might need reworking...
+        print(f"Attempting regex for {normalized_label}")
+        for alternative, standard in alt_field_map.items():
+            pattern = (
+                r"\b" + re.escape(alternative) + r"\b"
+            )  # Use word boundaries to match whole words
+            if re.search(pattern, normalized_label):
+                print(f"Found a regex match: {normalized_label} = {standard}")
+                return standard
+
+        # Attempt to find a match using regex without word boundaries for partial matches
+        print(f"Attempting regex for {normalized_label}")
+        for alternative, standard in alt_field_map.items():
+            if re.search(alternative, normalized_label):
+                print(f"Found a regex match: {normalized_label} = {standard}")
+                return standard
+
+        return None
+
     def fill_fields(self, driver):
         commons = Commons()
         csv_dir = os.path.join(os.getcwd(), "data", "input", "input_fields.csv")
@@ -28,11 +57,15 @@ class FormFiller:
         # print(input_details)
 
         # Fill in the fields
+        alt_map_dir = os.path.join(os.getcwd(), "config", "field_alternatives.csv")
+        alt_field_map = commons.alt_field_map(alt_map_dir)
+        # print(alt_field_map)
         for detail in input_details:
-            # Currently only matches "dumb" text
             label = detail["label"]
-            if label in csv_data:
-                value = csv_data[label]
+            matched_field = self.match_field_name(label, alt_field_map)
+            if matched_field and matched_field in csv_data:
+                value = csv_data[matched_field]
                 element = detail["element"]
-                element.clear()  # Clear any pre-filled value
+                element.clear()
                 element.send_keys(value)
+                print(f"Sent field input: {value}")
